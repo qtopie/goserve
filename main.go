8e173f0f3407afe113aac8c49fe61692cc0ae2d8
artificerpi <artificerpi@users.noreply.github.com>
2017-10-06 16:20:59 +0800

    Add port configurable feature

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 	"net/http"
+	"flag"
+	"log"
 )
 
+var (
+	port int
+)
+
+// func configureCORS(handler http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Add("Access-Control-Allow-Origin", "*")
+// 		handler.ServeHTTP(w, r)
+// 	})
+
+// }
+
+// func buildHandler() http.Handler {
+// 	var handler http.Handler
+// 	handler = http.DefaultServeMux
+// 	handler = configureCORS(handler)
+// 	return handler
+// }
+
+
+
 func main() {
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+	flag.IntVar(&port, "p", 7070, "specify the port to listen on")
+	flag.Parse()
 
-	port := ":7070"
-	fmt.Println("Serving at", port)
-	http.ListenAndServe(port, nil)
+	fsHandler := http.FileServer(http.Dir("."))
+	http.Handle("/", fsHandler)
 
+	addr := ":" + strconv.Itoa(port)
+	log.Println("Serving port", addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil{
+		log.Println("Error", err)
+	}
 }
